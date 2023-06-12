932a3dad81b82b497131afab9e337fa2da4d37b5
juanhernandezdeveloper <jualherac@gmail.com>
2023-06-12 12:28:20 -0500

    Configure React app for deployment to GitHub Pages one more time

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
@@ -9,8 +9,8 @@ import "./style/index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <Router basename="/">
       <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>
 );
